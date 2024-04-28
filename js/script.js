2330a66a157286345223abb2570a3da2c4d6b211
Juan Carlos <juancarlosgmr1460@gmail.com>
2024-04-28 11:30:29 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -92,7 +92,6 @@ function fecharReviewJarOfFiles(aberto) {
             albumdisplay = 'hidden';
             album.style.visibility = 'hidden';
             album.style.position = 'absolute';
-            console.log('cu')
         }
     }
 
@@ -161,7 +160,6 @@ function fecharReviewNevermind(aberto) {
             albumdisplay = 'hidden';
             album.style.visibility = 'hidden';
             album.style.position = 'absolute';
-            console.log('cu')
         }
     }
 
@@ -230,7 +228,6 @@ function fecharReviewLTT(aberto) {
             albumdisplay = 'hidden';
             album.style.visibility = 'hidden';
             album.style.position = 'absolute';
-            console.log('cu')
         }
     }
 
@@ -299,7 +296,6 @@ function fecharReviewCelebritySkin(aberto) {
             albumdisplay = 'hidden';
             album.style.visibility = 'hidden';
             album.style.position = 'absolute';
-            console.log('cu')
         }
     }
 
@@ -368,7 +364,74 @@ function fecharReviewCore(aberto) {
             albumdisplay = 'hidden';
             album.style.visibility = 'hidden';
             album.style.position = 'absolute';
-            console.log('cu')
+        }
+    }
+
+
+
+}
+
+function abrirReviewPurple(txt) {
+
+    if (aberto == true) {
+        fecharReviewPurple(aberto);
+
+        aberto = false;
+    }
+    else {
+
+
+
+        aberto = true;
+
+
+
+
+
+        var album = document.getElementById('localReviewPurple');
+
+
+
+        album.style.backgroundColor = '#cf9191';
+        album.style.display = 'block';
+        album.style.visibility = 'visible';
+        album.style.position = 'relative';
+        album.style.width = '300px';
+        album.style.height = '200px';
+        album.style.borderRadius = '20px';
+        album.style.padding = '8px';
+        album.style.marginLeft = '8px';
+
+        album.innerText = 'Review do Album: \n \n' + txt;
+
+        album.style.fontFamily = 'Arial, Helvetica, sans-serif';
+        album.style.fontSize = '16px';
+
+        console.log(aberto);
+    }
+
+
+}
+
+function fecharReviewPurple(aberto) {
+    if (aberto == false) {
+        album.style.visibility = 'visible';
+        console.log('já tá fechado')
+    }
+
+    else {
+        aberto = false;
+        var album = document.getElementById('localReviewPurple');
+
+
+        var albumdisplay = document.getElementById('localReviewPurple').style.display;
+
+        console.log(albumdisplay.toString())
+
+        if (albumdisplay.toString() == 'block') {
+            albumdisplay = 'hidden';
+            album.style.visibility = 'hidden';
+            album.style.position = 'absolute';
         }
     }
 
